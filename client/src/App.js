5bcf3ca2b7443787a054340bb20d4f53d09a917b
Ricky Lau <rlau@vols.utk.edu>
2023-06-26 03:45:08 -0400

    imported react, changed styling, and added notes

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap'; // {} means you're importing specific things from specific libaries 
+
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
