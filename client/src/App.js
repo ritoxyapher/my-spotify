052d26cb98737552fca6bce6238b3f32387646b1
Ricky Lau <rlau@vols.utk.edu>
2023-06-28 03:28:17 -0400

    switched to JSX. UI needs to be split into components? Event listeners not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
-
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap'; // {} means you're importing specific things from specific libaries 
+import Search from "./components/Search"
 
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Search></Search>
   );
 }
 
---
+++ client/src/components/Search.js
@@ -0,0 +1,37 @@
+import React from "react"; // this lets me write jsx since i dont have typescript installed
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup'; // Import these components from react-bootstrap 
+
+function Pressed(){
+
+    return console.log("Pressed");
+}
+
+function Search(){
+    
+    return(
+
+        <div> {/*Jsx elements need to have one parent element since jsx operates similarly to a function */}
+            
+            <h1>Search</h1> {/* Functions only return one thing. So we will wrap it and return this to be rendered*/}
+
+            <InputGroup className="mb-3">
+            <Form.Control
+                placeholder="What do you want to listen to?"
+                aria-label="What do you want to listen to?"
+                aria-describedby="basic-addon2"
+                />
+                <Button variant="outline-secondary" id="button-addon2" onClick={Pressed()}>
+                    Search
+                </Button>
+            </InputGroup>
+
+           
+            
+        </div> 
+
+    );
+}
+
+export default Search;
